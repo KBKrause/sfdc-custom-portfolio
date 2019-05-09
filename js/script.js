34ab6f48d65a3d8444529dd59e4437f2f3e8d9f4
Kevin Krause <kkrause@appirio.com>
2019-05-09 15:09:26 -0400

    Carousel slider init

--- js/script.js
+++ js/script.js
@@ -60,3 +60,17 @@ function showTabThree() {
   $("#tab-default-2").attr(
     "class", "slds-tabs_default__content slds-hide");
 }
+
+function selectCarouselTwo() {
+  $("#content-id-01").attr("aria-hidden", "true");
+  $("#content-id-01").children().eq(0).attr("tabindex", "-1");
+  $("#indicator-id-01").attr("class", "slds-carousel__indicator-action");
+  $("#indicator-id-01").attr("tabindex", "-1");
+
+  $("#content-id-02").attr("aria-hidden", "false");
+  $("#content-id-02").children().eq(0).attr("tabindex", "0");
+  $("#indicator-id-02").attr("class", "slds-carousel__indicator-action slds-is-active");
+  $("#indicator-id-02").attr("tabindex", "0");
+
+  $("#CAROUSEL_SLIDER").attr("style", "transform:translateX(-100%)");
+}
